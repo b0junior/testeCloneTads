d8bea9561a3a3dc1a3d66a23109ce258b5c9544e
CFJunior <arrobaprotonmail@protonmail.com>
2020-02-14 10:06:40 -0300

    Teste concluído

--- teste.c
+++ teste.c
@@ -2,7 +2,14 @@
 #include <stdio.h>
 
 int main(){
+	int inp = 0;
+	printf("Insira um número: ");
+	scanf("%i", &inp);
 
-
+	if(inp%2 == 0)
+		printf("O número inserido é par.\n");
+	else
+		printf("O número inserido é impar.\n");
+			
     return 0;
 }
